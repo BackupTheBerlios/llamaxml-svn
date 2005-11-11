cc1a9d6396a5cf3f9c92e3b97459a263b3be4b42
samalone <samalone@def31644-02db-0310-b7a1-ff11ef4a7401>
2005-11-11 19:19:53 +0000

    Added GPL copyrights

--- LifeBalance/branches/WindowsMobile/LlamaXML/Mac/Codewarrior10/LlamaXMLTest/ParseRSS.cpp
+++ LifeBalance/branches/WindowsMobile/LlamaXML/Mac/Codewarrior10/LlamaXMLTest/ParseRSS.cpp
@@ -2,6 +2,8 @@
 #include "LlamaXML/XMLException.h"
 #include <string>
 
+// This code compiles but is untested
+
 std::string ParseRSS(LlamaXML::InputStream & input, LlamaXML::TextEncoding desiredEncoding) {
     std::string output;
     
@@ -23,11 +25,13 @@ std::string ParseRSS(LlamaXML::InputStream & input, LlamaXML::TextEncoding desir
     							output += "title: ";
     							output += reader.ReadElementString(desiredEncoding);
     							output += "\n";
+								reader.Read();	// end element or empty element
                             }
                             else if (reader.IsStartElement("severity")) {
                                 output += "sev: ";
 							    output += reader.ReadElementString(desiredEncoding);
 							    output += "\n";
+								reader.Read(); // end element or empty element
                             }
                             else {
                                 reader.Skip();
