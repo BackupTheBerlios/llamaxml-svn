b7754775fe46da585064cd7835cd852147157991
samalone <samalone@def31644-02db-0310-b7a1-ff11ef4a7401>
2005-11-12 15:45:05 +0000

    Tweaked some files in GoLive

--- LifeBalance/branches/WindowsMobile/LlamaXML/Documentation/GoLive/htdocs/index.php
+++ LifeBalance/branches/WindowsMobile/LlamaXML/Documentation/GoLive/htdocs/index.php
@@ -4,32 +4,13 @@
 
 	<head>
 		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-		<title>BerliOS - Der Open-Source-Mediator</title>
+		<title>LlamaXML — compact XML parsing library</title>
 		<link href="berlios.css" type="text/css" rel="stylesheet" />
 	</head>
 
 	<body>
-		<table width="100%" border="0" cellspacing="0" cellpadding="2" bgcolor="#7B7B7B">
-			<tr>
-				<td><span class="maintitlebar">  <b><a class="maintitlebar" href="http://www.berlios.de/index.php.en">Home</a></b> | <b><a class="maintitlebar" href="http://developer.berlios.de/about.php">About us</a></b> | <b><a class="maintitlebar" href="http://developer.berlios.de/partners.php">Partners</a></b> | <b><a class="maintitlebar" href="http://developer.berlios.de/contact.php">Contact</a></b></span></td>
-			</tr>
-		</table>
-		<table width="100%" border="0" cellspacing="0" cellpadding="0">
-			<tr valign="top" bgcolor="#FFCC33">
-				<td bgcolor="#FFFFFF"><a href="http://www.berlios.de/index.php.en"><img src="http://developer.berlios.de/images/berliOS_logo.png" alt="BerliOS" height="61" width="238" border="0" hspace="5" vspace="5" /></a></td>
-				<td bgcolor="#FFCC33" width="10"><img src="http://developer.berlios.de/images/blank.gif" alt="" height="1" width="10" border="0" /></td>
-				<td valign="middle" width="99%"><b><font size="+1">BerliOS</font></b><br />
-					The Open Source Mediator</td>
-				<!-- logo at right -->
-				<td valign="middle"><a href="http://www.fokus.fraunhofer.de/" target="_blank"><img src="http://developer.berlios.de/images/logo_fokus.png" alt="FOKUS" height="60" width="60" align="right" border="0" hspace="10" /></a></td>
-			</tr>
-			<tr>
-				<td colspan="4" bgcolor="#7B7B7B"><img src="http://developer.berlios.de/images/blank.gif" alt="" height="2" width="2" /></td>
-			</tr>
-		</table>
-		
 		<div class="content">
-		<csobj occur="54" w="600" h="132" t="Component" csref="../htdocs.data/Components/directory.html">
+		<csobj occur="54" w="578" h="132" t="Component" csref="../htdocs.data/Components/directory.html">
 				<div class="directory">
 					<ul>
 						<li><a href="index.php">Home</a></li>
@@ -50,7 +31,7 @@
 			<p>LlamaXML is a C++ XML parsing and formatting library that is small enough to be used on handheld devices like PDAs and smartphones, while still supporting larger desktop and server systems. The library currently runs on Palm OS Garnet, Palm OS Cobalt, Windows Mobile, Windows and Mac OS X, and is easy to port to new platforms. The entire library compiles to only 45 KB of code under Codewarrior for Palm OS, and can be even smaller if only some of its features are used. The library is also frugal with runtime memory, using a single-pass streaming algorithm that only stores the current parsing stack in memory, allowing very large XML documents to be parsed with very little overhead.</p>
 			<p>The LlamaXML library supports XML namespaces and automatic encoding conversion. It does not support validation, XML schemas, DTDs, or external entities. If you need these features, consider using <a href="http://xmlsoft.org/">libxml2</a> or <a href="http://xml.apache.org/xerces-c/">Xerces</a>.</p>
 			<p>LlamaXML is distributed under a dual licensing system in order to support both open source and commercial developers. Open source developers may use LlamaXML under the Gnu General Public License. Commercial developers can purchase a commercial license to use LlamaXML in their own projects without having to distribute their own source code.</p>
-			<p>The project is hosted by <a href="http://developer.berlios.de"><img src="http://developer.berlios.de/bslogo.php?group_id=0&amp;type=1" alt="BerliOS Logo" height="32" width="124" border="0" /></a></p>
+			<p>The project is hosted by <a href="http://developer.berlios.de"><img src="http://developer.berlios.de/bslogo.php?group_id=0&amp;type=1" alt="BerliOS Logo" height="32" width="124" align="absmiddle" border="0" /></a></p>
 		
 		
 		<p><a href="http://validator.w3.org/check?uri=referer"><img
@@ -61,12 +42,6 @@
        alt="Valid CSS!" border="0" />
  </a></p>
  </div>
- 
-		<table width="100%" border="0" cellspacing="0" cellpadding="2" bgcolor="#7B7B7B">
-			<tr>
-				<td align="center"><span class="titlebar"><font color="#FFFFFF">Copyright © 2000-<?php echo date("Y"); ?> <a class="maintitlebar" href="http://www.fokus.fraunhofer.de/" target="_blank">FOKUS</a></font></span></td>
-			</tr>
-		</table>
 	</body>
 
 </html>
