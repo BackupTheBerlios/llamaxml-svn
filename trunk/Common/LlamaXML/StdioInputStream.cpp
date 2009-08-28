1039e4d0ac36184c2ce636841f03c347201bbb30
samalone <samalone@def31644-02db-0310-b7a1-ff11ef4a7401>
2009-08-28 20:33:27 +0000

    Added StdioInputStream and StdioOutputStream classes, which should work just about everywhere but Palm OS.

---
+++ trunk/Common/LlamaXML/StdioInputStream.cpp
@@ -0,0 +1,43 @@
+/*
+ *  StdioInputStream.cpp
+ *  BuddySystem
+ *
+ *  Created by Stuart A. Malone on 8/28/09.
+ *  Copyright 2009 Llamagraphics, Inc. All rights reserved.
+ *
+ */
+
+#include "LlamaXML/StdioInputStream.h"
+#include "LlamaXML/XMLException.h"
+
+namespace LlamaXML {
+
+	StdioInputStream::StdioInputStream(const char * path)
+	: mFile(fopen(path, "rb"))
+	{
+		if (! mFile) {
+			ThrowXMLError(errno);
+		}
+	}
+	
+	StdioInputStream::~StdioInputStream() {
+		fclose(mFile);
+	}
+
+	uint32_t StdioInputStream::ReadUpTo(char * buffer, uint32_t length) {
+		size_t bytesRead = fread(buffer, sizeof(*buffer), length, mFile);
+		if (ferror(mFile)) {
+			ThrowXMLError(errno);
+		}
+		return bytesRead;
+	}
+		
+	void StdioInputStream::Restart() {
+		rewind(mFile);
+	}
+
+	bool StdioInputStream::EndOfFile() {
+		return feof(mFile) != 0;
+	}
+
+}
---
+++ trunk/Common/LlamaXML/StdioOutputStream.cpp
@@ -0,0 +1,34 @@
+/*
+ *  StdioOutputStream.cpp
+ *  BuddySystem
+ *
+ *  Created by Stuart A. Malone on 8/28/09.
+ *  Copyright 2009 Llamagraphics, Inc. All rights reserved.
+ *
+ */
+
+#include "LlamaXML/StdioOutputStream.h"
+#include "LlamaXML/XMLException.h"
+
+namespace LlamaXML {
+
+	StdioOutputStream::StdioOutputStream(const char * path)
+	: mFile(fopen(path, "wb"))
+	{
+		if (! mFile) {
+			ThrowXMLError(errno);
+		}
+	}
+	
+	StdioOutputStream::~StdioOutputStream() {
+		fclose(mFile);
+	}
+	    
+	void StdioOutputStream::WriteData(const char * buffer, uint32_t length) {
+		size_t bytesWritten = fwrite(buffer, sizeof(*buffer), length, mFile);
+		if (bytesWritten < length) {
+			ThrowXMLError(errno);
+		}
+	}
+	
+}
